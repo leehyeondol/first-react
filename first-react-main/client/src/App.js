9b60ecb559cbda9afb6c8b6b0c9b408ea4d09935
hyeondo <guseh5634@gmail.com>
2023-08-25 00:06:28 +0900

    일단 로그인 수정

--- first-react-main/client/src/App.js
+++ first-react-main/client/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useCookies } from "react-cookie";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [isRemember, setIsRemember] = useState(false);
+  const [cookies, setCookie, removeCookie] = useCookies(["rememberText"]);
+  //cookies는 쿠키의 정의,
+  //setCookie는 쿠키를 재정의,
+  //removeCookie는 쿠키 제거이다.
+  //useCookies 안에는 초기값을 넣는다.
+
+  let now = new Date();
+  let after1m = new Date();
+
+  useEffect(() => {
+    if (cookies.rememberText !== undefined) {
+      setText(cookies.rememberText);
+      setIsRemember(true);
+    }
+  });
+
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  const handleOnChange = (e) => {
+    after1m.setMinutes(now.getMinutes() + 1); // after1m를 현재시간의 1분뒤로 정의
+    setIsRemember(e.target.checked);
+    if (e.target.checked) {
+      setCookie("remeberText", text, { path: "/", expires: after1m });
+      // remeberText에 text라는 값을 넣는다.
+      // path는 적용되는 도메인
+      // expires = 만료시간은 1분뒤
+    } else {
+      removeCookie("rememberText");
+      // checkBox의 체크를 지울시
+      // 쿠키 remeberText를 지운다
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <input value={text} onChange={onChange} />{" "}
+      <input type="checkBox" onChange={handleOnChange} checked={isRemember} />{" "}
+      <h1>{text}</h1>{" "}
+    </>
   );
 }
 
